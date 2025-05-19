3d525bc33f3a50ece38a392d5c503bcc5248d858
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-05-18 23:18:07 -0700

    new

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import services from '../navigation/services'
 
 const Main = () => {
   return (
@@ -7,7 +8,7 @@ const Main = () => {
             {/* <img src="/background.jpg" alt="main" className='w-full h-[100vh] object-cover' /> */}
             <div className='flex justify-center items-center h-[70vh]  bg-cover bg-no-repeat bg-center' style={{ backgroundImage: "url('/background.jpg')" }}>
               <div className='flex items-center bg-black/50 w-screen h-full justify-center px-4'>
-              <div className='flex flex-col'>
+              <div className='flex flex-col '>
                     <h1 className='text-3xl font-bold text-white text-wrap md:w-130'>We Build Websites & Web Apps That Drives Results</h1>
                     <p className='font-semibold pt-2 text-white'>Tailored digital Solutions for business ready to grow</p>
 
@@ -24,6 +25,23 @@ const Main = () => {
             </div>
         </div>
 
+
+        <div className='pb-8'>
+            <h1 className='text-3xl font-bold text-center pt-10'>Our Services</h1>
+            <p className='text-center pt-1 text-gray-500 text-sm'>We offer a wide range of services to help you achieve your digital goals.</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 px-4 pt-10'>
+              {services.map((items) => (
+                
+                  <div className='bg-white shadow-2xl px-4 rounded-md py-6'>
+                    <div>
+                        {items.icon}
+                    </div>
+                  <h2 className='text-xl font-bold text-center'>{items.name}</h2>
+                  <p className='pt-2 text-center text-gray-600 text-sm'>{items.description}</p>
+              </div>
+              ))}
+                </div>
+        </div>
     </div>
   )
 }
