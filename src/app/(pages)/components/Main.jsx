09f39168f9271d0d67a7c39292c0afbedd18fbc7
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-05-21 14:03:16 -0700

    new update

--- src/app/(pages)/components/Main.jsx
+++ src/app/(pages)/components/Main.jsx
@@ -4,29 +4,14 @@ import Contactus from './Contactus'
 import about, { vision } from '../navigation/about'
 import Aboutus from './aboutus'
 import Footer from './Footer'
+import Hero from './hero'
 
 const Main = () => {
   return (
     <div>
         <div className="img">
             {/* <img src="/background.jpg" alt="main" className='w-full h-[100vh] object-cover' /> */}
-            <div className='flex justify-center items-center h-[70vh]  bg-cover bg-no-repeat bg-center' style={{ backgroundImage: "url('/background.jpg')" }}>
-              <div className='flex items-center bg-black/50 w-screen h-full justify-center px-4'>
-              <div className='flex flex-col '>
-                    <h1 className='text-3xl font-bold text-white text-wrap md:w-130'>We Build Websites & Web Apps That Drives Results</h1>
-                    <p className='font-semibold pt-2 text-white'>Tailored digital Solutions for business ready to grow</p>
-
-                    <div className='pt-9 md:pt-5'>
-                        {/* <button className='bg-blue-900 text-white px-4 py-2 rounded-md mt-4'>Contact us</button> */}
-                        <button className='bg-blue-900 text-white px-4 py-2 rounded-md mt-4'>See Our Services</button>
-                    </div>
-                    <p className='pt-9 text-white'>Innovative. Simple. Fast </p>
-                </div>
-                <div className='hidden md:flex'>
-                    <img src="/side.png" alt="main" className='w-full  object-cover scale-100' />
-                    </div>
-              </div>
-            </div>
+           <Hero/>
         </div>
 
 
--- src/app/(pages)/navigation/about.js
+++ src/app/(pages)/navigation/about.js
@@ -2,7 +2,7 @@ var about;
 
 export default about =[
     {
-        title: "About",
+        title: "About Us",
         subtitle: "Our mission is to meet peoples needs in ways they never imagined possible.",
         text: "is a fast growing technology company that specialize in software development. We focus on developing technology solutions that helps our clients achieve their ultimate goals in ways they never imagined. We develop software solutions, web based solutions, we engage in digital marketing,and other forms of digital solutions."
     }
